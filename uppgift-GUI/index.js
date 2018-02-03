9fd659ba68b5132c1695b773d818b76807646cba
Jenny Miderkvist <jennymider@outlook.com>
2018-02-03 17:39:33 +0100

    added notes

--- uppgift-GUI/index.js
+++ uppgift-GUI/index.js
@@ -1,4 +1,5 @@
 ﻿var xhttp = new XMLHttpRequest();
+
 xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         // Typical action to be performed when the document is ready:
@@ -8,7 +9,21 @@ xhttp.onreadystatechange = function () {
             document.getElementById('notes').innerHTML += '<option value="' + JSON.stringify(note._id) +
                 '">' + JSON.stringify(note.title) + '</option>';
         }
-        //document.getElementById('notes').innerHTML = xhttp.response;
+
+        //Vi vill ha vår array med data att skicka in i vår ViewModel nedanför
+
+        var ViewModel = function (notes) {
+            this.wishes = ko.observableArray(notes);
+            this.addWish = function () {
+                this.wishes.push({ title: "" });
+            };
+            this.removeWish = function (wish) {
+                this.wishes.remove(wish);
+            };
+        };
+
+        ko.applyBindings(new ViewModel([{ title: "AAA" }]));
+        
     };
 };
 xhttp.open("GET", "http://localhost:8088/nodes", true);
