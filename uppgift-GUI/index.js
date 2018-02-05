33a1d6ca941e86122a1a60dd3a383278e00af7ce
Jenny Miderkvist <jennymider@outlook.com>
2018-02-05 16:00:39 +0100

    added post

--- uppgift-GUI/index.js
+++ uppgift-GUI/index.js
@@ -3,8 +3,13 @@
 xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         // Typical action to be performed when the document is ready:
+        console.log(xhttp.response);
+
         var inData = JSON.parse(xhttp.response);
+        console.log(inData[0].title);
+
         ko.applyBindings(new viewModel(inData));
+        console.log(document.getElementById('dropdown').value);
     };
 };
 
@@ -13,5 +18,16 @@ xhttp.send();
 
 var viewModel = function (data) {
     this.notes = ko.observableArray(data);
+
+    this.note = {
+        _id: ko.observable(''),
+        title: ko.observable(''),
+        text: ko.observable('')
+    }
 }
 
+function showNote() {
+    var element = document.getElementById('dropdown');
+    document.getElementById('noteTitle').value = element.options[element.selectedIndex].text;
+    document.getElementById('noteBody').value = element.options[element.selectedIndex].value;
+}
